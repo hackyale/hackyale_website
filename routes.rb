e4956a64a602d2e17ff2d2578de0ead7ed63e398
Zack Reneau-Wedeen <xaq2892@yahoo.com>
2012-09-13 13:22:23 -0400

    Add pdf for week_0 slides

--- routes.rb
+++ routes.rb
@@ -10,8 +10,8 @@ get route_rgx do
   redirect "/#" + params[:captures].first.downcase
 end
 
-#map all stylesheets to sass
+#map all stylesheets to scss
 get '/stylesheets/:name.css' do
- content_type 'text/css', :charset => 'utf-8'
- scss(:"/../public/stylesheets/#{params[:name]}")
+  content_type 'text/css', :charset => 'utf-8'
+  scss :"/../public/stylesheets/#{params[:name]}"
 end
